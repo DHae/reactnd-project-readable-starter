b5eb32d4d0b9c4a32a09b1e9e657fb439228c203
David Haerle <david.haerle@logicline.de>
2017-11-12 16:26:39 +0100

    feat: Add react-router

--- readable/src/components/App.js
+++ readable/src/components/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import { Router } frm 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          Empty project
-        </p>
+        <Router exact path='/' render {() => (
+          <p className="App-intro">
+            Empty project
+          </p>
+        )} />
       </div>
     );
   }
